380bae7df7046acfb288dad27472e7aac3a78d08
Prabhsimran Singh <psimran.singh@gmail.com>
2021-09-27 21:54:50 -0400

    Added code for managing the development data set Removed APRED because might not use, put in seperate file

--- Data Management.R
+++ Data Management.R
@@ -2,12 +2,13 @@ setwd("~/Rutgers Fall 2021/Theory and Practice of Public Informatics/")
 
 library(dplyr)
 library(tidyverse)
+library(readr)
 
 ###IMPORT DATA FILES###
 project_sunroof_county <- read.csv("Solar Data Files/project-sunroof-county.csv")
 pop_by_race <- read.csv("Solar Data Files/StatsAmerica Datasets/Population-by-Race/Population by Race - US, States, Counties.csv")
 pop_by_age_sex <- read.csv("Solar Data Files/StatsAmerica Datasets/Population-by-Age-and-Sex/Population by Age and Sex - US, States, Counties.csv")
-apred0 <- read.csv("Solar Data Files/StatsAmerica Datasets/APRED/APRED - Disaster Resilience - Counties.csv")
+metrics_dev1 <- read.csv("Solar Data Files/StatsAmerica Datasets/Metrics-For-Development/Metrics For Development.csv")
 
 ###LIMIT ALL DATASETS TO SELECT STATES AND YEARS###
 northeast_sunroof <- subset(project_sunroof_county,
@@ -42,16 +43,24 @@ pop_by_age_sex3 <- rbind(pop_by_age_sex3, dplyr::filter(pop_by_age_sex2,grepl(',
 
 pop_demographics <- merge(pop_by_age_sex3,pop_by_race3)
 
-apred2 <- subset(apred0,
-                 apred$Year=="2019",
-                 select=c(1:11))
+metrics_dev2 <- subset(metrics_dev1,
+                 metrics_dev1$Year=="2019",
+                 select=c(1:8))
 
-apred3 <- subset(apred2, apred2$State.Abbreviation=='NJ' |
-                         apred2$State.Abbreviation=='NY' |
-                         apred2$State.Abbreviation=='CT' |
-                         apred2$State.Abbreviation=='PA' |
-                         apred2$State.Abbreviation=='RI',
-                 select=c(1:11))
+metrics_dev3 <- data.frame()
+metrics_dev3 <- rbind(metrics_dev3, dplyr::filter(metrics_dev2,grepl(', NY',metrics_dev2$Description)))
+metrics_dev3 <- rbind(metrics_dev3, dplyr::filter(metrics_dev2,grepl(', NJ',metrics_dev2$Description)))
+metrics_dev3 <- rbind(metrics_dev3, dplyr::filter(metrics_dev2,grepl(', PA',metrics_dev2$Description)))
+metrics_dev3 <- rbind(metrics_dev3, dplyr::filter(metrics_dev2,grepl(', CT',metrics_dev2$Description)))
+metrics_dev3 <- rbind(metrics_dev3, dplyr::filter(metrics_dev2,grepl(', RI',metrics_dev2$Description)))
+
+metrics_dev4 <- metrics_dev3[c(3:8)]
+
+#pick out select variables
+metrics_dev5 <- data.frame()
+#snap benefits per capita
+metrics_dev5 <- rbind(metrics_dev5, dplyr::filter(metrics_dev4,M4D_Code==10200)
+#
 
 ###REFORMAT COUNTY AND STATE VARIABLES TO MERGE ALL DATASETS###
 apred3$County.Name <- str_c(apred3$Description, " County")
@@ -73,39 +82,19 @@ pop_demographics <- pop_demographics[c(4:25)]
 pop_dem_pct <- pop_demographics
 pop_dem_pct[, -(1:3)] <- sweep(pop_dem_pct[, -(1:3)], 1, pop_dem_pct[, 3], "/")
 
-apred4 <- apred3[c(1,5,6,9,10)]
-  
-# Educational Equity
-# Age
-# Transportation Access
-# Communication Capacity
-# Language Capacity
-# Special Needs
-# Health Coverage
-# Housing Capital
-# Employment
-# Income & Equality (GINI Coefficient)
-# Single Sector Employment Dependence
-# Employment (Female)
-# Housing Type
-# Shelter Capacity
-# Housing Age
-# Sheltering Need
-# Place Attachment ? Migration
-# Place Attachment ? Born
-# Social Capital ? Religion
-# Social Capital ? Civic Involvement
-# Social Capital ? Advocacy
+namelist <- unique(metrics_dev4$M4D_Code)
+t_metrics_dev0 <- t(metrics_dev4[1:228,6])
+colnames(t_metrics_dev0) <- namelist
+t_metrics_dev <- cbind(metrics_dev4[1,2:3],t_metrics_dev0)
+names(t_metrics_dev) <- namelist
 
+aaa=t(apred4[1:21,6])  ### so this code extract the values from 1-21 rows and 6th column and tranpose it to 1 row 21 columns
+aaa=rbind(aaa,t(apred4[22:42,6]))  ### you can write a loop to replace the 22:42 here, so that R can automatically extract every 21 rows
+aaa  ### I just wanted to show you an example of how the output looks like
 
-  for (i in unique(data3$hhid)){
-    temp=data3[which(data3$hhid==i),]
-    temp$oldest_child[which(temp$age==max(temp$age))]=TRUE
-    temp$oldest_male_child[which(temp$oldest_child==1 & temp$sex=="male")]=TRUE
-    data3a=rbind(data3a,temp)
-  }
   
 rm(pop_by_age_sex,pop_by_age_sex2,pop_by_age_sex3)
 rm(pop_by_race,pop_by_race2,pop_by_race3)
 rm(project_sunroof_county)
-rm(apred0,apred2)
+rm(apred1,apred2)
+rm(metrics_dev0,metrics_dev1,metrics_dev2,metrics_dev3,metrics_dev5)
