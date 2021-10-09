0dd298f6cc08f71d4e1a0fde3eb221039e059358
Prabhsimran Singh <psimran.singh@gmail.com>
2021-10-09 12:52:04 -0400

    initial commit of data exploration file for TX and CA only

---
+++ Data Exploration 3 (TX and CA).R
@@ -0,0 +1,79 @@
+#RUN DATA MANAGEMENT CODE FIRST
+library(ggplot2)
+library(dplyr)
+library(gtsummary)
+library(psych)
+library(corrplot)
+library(xtable)
+library(knitr)
+library(ggbiplot)
+
+summary(solar_data4)
+
+solar_data4b <- solar_data4
+colnames(solar_data4b) <- c("Description",
+                           "County Name",
+                           "State",
+                           "% Solar Panel Installed",
+                           "Median KW Potential",
+                           "% w/ Health Insurance",
+                           "Property Crime Rate",
+                           "Rural/Urban Score",
+                           "% w/ Bachelor's Degree",
+                           "Population Density",
+                           "% Children w/ Single Parent",
+                           "% Population Age 45-64",
+                           "% Female",
+                           "% White",
+                           "Income per Capita",
+                           "Entrepeneurship Score",
+                           "Belief in Science Score",
+                           "Risk Taking Score",
+                           "Religiosity Score")
+
+solar_data5 <- solar_data4b[c(4:19)]
+
+###Some last minute transformations to normalize our variables
+solar_data5b <- solar_data5
+solar_data5b$"Population Density" <- (solar_data5$"Population Density")^(.5)
+plot(density(solar_data5b$"Population Density"))
+solar_data5b$"% White" <- log(solar_data5$"% White")
+plot(density(solar_data5b$"% White"))
+
+##Principal Components
+pca <- prcomp(solar_data5, center=TRUE, scale.=TRUE)
+pca
+ggbiplot(pca)
+
+pca2 <- principal(solar_data5, rotate="none", nfactors=16, scores=TRUE)
+pca3 <- principal(solar_data5, rotate="varimax", nfactors=16, scores=TRUE)
+pca2
+pca3
+
+#Correlation Matrix
+solar_data5.cor = cor(solar_data5)
+solar_data5.cor
+
+jpeg("CorrPlot.jpeg", width = 10, height = 10, units = 'in', res = 300)
+corrplot(solar_data5.cor,
+         type="full",
+         method="shade",
+         addCoef.col = TRUE,
+         tl.cex=1,
+         tl.col="black",
+         number.cex=.75,
+         number.digits=2,
+         sig.level=TRUE,
+         cl.cex=1,
+         tl.srt=45,
+         addgrid.col="black",
+         order="FPC")
+dev.off()
+
+#Factor Analysis
+solar.fa <- factanal(solar_data5,factors=5)
+solar.fa
+summary(solar.fa)
+
+lm <- lm(prop_crimes ~ White.Alone + pct_bachelors + income_per_capita, data = solar_data5)
+summary(lm)
